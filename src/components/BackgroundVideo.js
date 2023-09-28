2e94d918e56f5c8dc1c26d52e14f1fc308772dfd
tolgabaydarr <tolga.baydar@valensas.com>
2023-09-29 01:44:50 +0300

    Video playsInline added

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -2,7 +2,7 @@ export const BackgroundVideo = () => {
 
     return (
         <div className="background-video">
-            <video autoPlay loop muted>
+            <video autoPlay loop muted playsInline>
                 <source src="/BgVideo.mp4" type="video/mp4" />
                 {/* Add more source elements for different video formats if needed */}
             </video>
